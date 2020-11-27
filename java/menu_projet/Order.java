81a3e0a20ac5a816195b0775fa5476f5e19075f0
sulft <vilver.terry@gmail.com>
2020-11-27 12:57:23 +0100

    Menu partie 2 (plus élaboré prends en compte le choix effectué)

--- java/menu_projet/Order.java
+++ java/menu_projet/Order.java
@@ -4,10 +4,28 @@ public class Order {
         System.out.println("1 - poulet");
         System.out.println("2 - boeuf");
         System.out.println("3 - végétarien");
-        System.out.println("Que souhaitez-vous comme menu ?");
+        System.out.print("Que souhaitez-vous comme menu ? ");
     }
 
     public void affichageMenuSelectioner (int choix) {
-        System.out.println("Vous avez choisi le menu " + choix + " !");
+        System.out.print("\n");
+        switch(choix) {
+
+            case 1:
+                System.out.println("Vous avez choix le menu " + choix + " - Poulet");
+                break;
+            
+            case 2:
+                System.out.println("Vous avez choix le menu " + choix + " - Boeuf");
+                break;
+
+            case 3:
+                System.out.println("Vous avez choix le menu " + choix + " - Végétarien");
+                break;
+            
+            default:
+                System.out.println("Erreur sur le choix !");
+                
+        }
     }
 }
