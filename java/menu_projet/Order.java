662798c5575b03dc3eaa6bbab454512b8b11eb5f
vilver <vilver.terry@gmail.com>
2020-11-30 11:47:40 +0100

    avancé projet bug de tableau à résoudre

--- java/menu_projet/Order.java
+++ java/menu_projet/Order.java
@@ -4,23 +4,14 @@ public class Order {
     static Scanner scan = new Scanner(System.in);
     private int choix; //choix commande
     private int nbCommande; //nombre de commande
-    //Affiche tous les menus
-    public void affichageMenu () {
-        System.out.print("\n");
-        System.out.println("1 - poulet");
-        System.out.println("2 - boeuf");
-        System.out.println("3 - végétarien");
-        System.out.print("Que souhaitez-vous comme menu ? ");
-    }
-
 
     public void demanderMenu(String categorie, String[] reponse) {
         System.out.println("Choix " + categorie); // affiche le choix de la catégorie en cours
-        for(int i = 0; i < reponse.length; i++) {
-            System.out.println(i+1 + " - " + reponse[i]);
-        }
+            for(int i = 0; i < reponse.length; i++) {
+                System.out.println(i+1 + " - " + reponse[i]);
+            }
 
-        System.out.println("Que voulez-vous comme " + categorie + " ? ");
+        System.out.print(categorie + " ? ");
 
         boolean ok; //verifie si la commande est bien effectué 
 
@@ -35,28 +26,33 @@ public class Order {
 
             if(ok == true) {
                 System.out.println("Vous avez choisi " + categorie + " : " + reponse[this.choix - 1]);
+                System.out.println("\n");
             }
             else {
                 System.out.println("Vous n'avez pas choisi de " + categorie + ".." );
+                System.out.println("\n");
             }
         }while(ok==false);
     }
 
     public void affichageMenuSelectionner(int choix) {
         String accompagnement[] = {"Légume", "Frite", "Riz"};
+        String boisson[] = {"Eau plate", "Eau gazeux", "Soda"};
         if(choix == 1) {
             this.demanderAccompagnement(true, "Accompagnement", accompagnement);
+            this.demanderBoisson("Boisson", boisson);
+
         } else if (choix == 2) {
             this.demanderAccompagnement(true, "Accompagnement", accompagnement);
         } else {
             this.demanderAccompagnement(false, "Accompagnement", accompagnement);
+            this.demanderBoisson("Boisson", boisson);
         }
     }
 
     public void demanderAccompagnement(boolean accompagnementPossible, String categorie, String[] reponse ) {
         if(accompagnementPossible) {
-            String accompagnement[] = {"Poulet", "Boeuf", "Végétarien"};
-            this.demanderMenu("Accompagnement", accompagnement);
+            this.demanderMenu(categorie, reponse);
         } else {
             String rizOuNon[] = {"Oui", "Non"};
             this.demanderMenu("Riz", rizOuNon);
@@ -64,40 +60,14 @@ public class Order {
 
     }
 
-    public void boisson() { 
-        do {
-            System.out.print("\n");
-            System.out.println("1 - Eau plate");
-            System.out.println("2 - Eau gazeuse");
-            System.out.println("3 - Soda");
-            System.out.print("Que souhaitez-vous comme boisson ? ");
-
-            this.choix = scan.nextInt();
-
-            switch(this.choix) {
-                case 1:
-                    System.out.println("Vous avez fait le choix de la boisson " + this.choix + " - Eau plate.");
-                    break;
-            
-                case 2:
-                    System.out.println("Vous avez fait le choix de la boisson " + this.choix + " - Eau gazeuse.");
-                    break;
-
-                case 3:
-                    System.out.println("Vous avez fait le choix de la boisson " + this.choix + " - Soda.");
-                    break;
-            
-                default:
-                    System.out.println("Vous n'avez pas choisi de boisson");
-            }
-        }while(this.choix<1 ||this.choix>3);
+    public void demanderBoisson(String categorie, String[] reponse) {
+        this.demanderMenu(categorie, reponse);
     }
 
     public void runMenu () {
         String menu[] = {"Poulet", "Boeuf", "Végétarien"};
         this.demanderMenu("Menu", menu);
         this.affichageMenuSelectionner(this.choix);//sélectionne le choix entrer dans demanderMenu
-        //this.affichageMenuSelectionner();
     }
 
     public void nombreDeCommande() {
@@ -105,8 +75,6 @@ public class Order {
         this.nbCommande = scan.nextInt();
         for(int i = 0; i<this.nbCommande; i++) {
             runMenu();
-            System.out.print("\n");
-            System.out.print("\n");
             if(i<this.nbCommande-1) {
                 System.out.print("Ok c'est noté, commande suivante ? ");
                 System.out.print("\n");
