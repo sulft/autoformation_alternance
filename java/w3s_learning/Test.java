96609478cc91076c17e312dc9e6da1c90a79e055
sulft <vilver.terry@gmail.com>
2020-12-02 16:22:21 +0100

    recursivite

--- java/w3s_learning/Test.java
+++ java/w3s_learning/Test.java
@@ -7,7 +7,19 @@ public class Test {
     static double addition(double a,double b) {
         return a+b;
     }
+
+    static int recursivite(int a) {
+        if(a>3){
+            System.out.println(a);
+            return a + recursivite(a-1);
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String []args) {
         System.out.println(addition(5,3) +" et " + addition(3.2,2.4) + "! Ici je surcharge la fonction addition");
+        System.out.print("\n");
+        System.out.print(recursivite(6));
     }
 }
