207870aacd578c4d02cdb01b4be18ee78cc79094
sulft <vilver.terry@gmail.com>
2020-12-02 10:00:04 +0100

    learning to write in a file with java

--- java/menu_projet/Test.java
+++ java/menu_projet/Test.java
@@ -1,25 +1,23 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException; 
+
+//text package
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.OutputStreamWriter;
 
 
 public class Test {
     public static void main(String [] args) {
+        String text ="choco, lait, oeuf";
         try {
-            File test = new File("test.txt");
-            if (test.createNewFile()) {
-                System.out.println("File created: " + test.getName());
-                test.write("Ce code est me permet d'apprendre à écrire dans un fichier text à l'aide de java");
-                test.close();
-            } else {
-                System.out.println("File already exists.");
-            }
+            //FileWriter w = new FileWriter("test.txt", true);
+            FileOutputStream oStream = new FileOutputStream("test.txt",true);
+            OutputStreamWriter sWriter = new OutputStreamWriter(oStream);
+            BufferedWriter bWriter = new BufferedWriter(sWriter);
+            sWriter.write("Ce code est me permet d'apprendre à écrire dans un fichier text à l'aide de java\n");
+            sWriter.write(text + "\n");
+            sWriter.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
