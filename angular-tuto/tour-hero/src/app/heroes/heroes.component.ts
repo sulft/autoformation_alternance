ca7a8ae130e0e636df428751ce28aedcfa39b490
vilver <vilver.terry@gmail.com>
2020-12-12 23:39:50 +0100

    tower of hero

---
+++ angular-tuto/tour-hero/src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { hero } from '../hero'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero : hero = {
+    id : 1,
+    name : "Windstorm"
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
